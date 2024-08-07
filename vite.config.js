40632a13c9729e4888a12809bdcce676ba84344c
Ilyosbek0097 <i_s_0097@mail.ru>
2024-08-07 14:02:21 +0500

    Nol Shablon Docker php8.3, nginx, vite, pgsql, pgadmin, inertia, vue, vitejs, brezze

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: process.env.VITE_HOST || '0.0.0.0',
+        port: parseInt(process.env.VITE_PORT) || 5173,
+        https: process.env.VITE_HTTPS === 'true',
+        hmr: {
+            host: process.env.VITE_HMR_HOST || 'localhost',
+            port: parseInt(process.env.VITE_ADDITIONAL_PORT) || 5173,
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+
 });
